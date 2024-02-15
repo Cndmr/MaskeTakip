2cc631b2233987bf677f930d6823575c809a210d
Rabia <84686864+Cndmr@users.noreply.github.com>
2024-02-15 03:08:33 +0300

    Add project files.

---
+++ Workaround/Program.cs
@@ -0,0 +1,12 @@
+﻿using Entities.Concrete;
+using Business.Concrete;
+
+Person person1 = new Person();
+person1.FirstName = "Can";
+person1.LastName = "Demir";
+person1.DateOfBirthYear = 1999;
+person1.NationalIdentity = 29200887310;
+
+PttManager pttManager = new PttManager(new PersonManager());
+pttManager.GiveMask(person1);
+Console.ReadLine();
